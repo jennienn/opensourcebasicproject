127c940e58872b123af4d9077f84f7f79f38666f
jennienn <118978246+jennienn@users.noreply.github.com>
2023-05-29 19:34:12 +0900

    Update create_table(final).sql
    mail_table 수정 (title 추가, content 크기 수정)

--- create_table(final).sql
+++ create_table(final).sql
@@ -10,10 +10,12 @@ CREATE TABLE `user_table` (
 
 CREATE TABLE `mail_table` (
   `mail_id` int NOT NULL AUTO_INCREMENT,
-  `content` varchar(45) NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `content` varchar(1024) NOT NULL,
   `created_at` varchar(45) NOT NULL,
   `weather` int NOT NULL,
   `user_id` int DEFAULT NULL,
-  `send` bool DEFAULT False,
+  `send` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`mail_id`)
-  )
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+SELECT * FROM opensource.mail_table;
